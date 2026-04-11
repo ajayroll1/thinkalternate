c4635999ece29413d7c69356c34ef7e9fc47a2c3
AJAY MAHTO <ajaymahto2580@gmail.com>
2026-04-11 06:28:46 +0000

    yes

--- pages/AdminPanel/jsobjects/JSObject2/JSObject2.js
+++ pages/AdminPanel/jsobjects/JSObject2/JSObject2.js
@@ -1,5 +1,17 @@
 export default {
-	TicketsTableprimaryColumnsStatusmenuItemsmenuItemkcq0vdtq9ionClick () {
-		//	write code here
-	}
+  async updateStatus(status) {
+
+    const row = TicketsTable.triggeredRow;
+
+    // 1. Google Sheet update
+    await Api2.run({ status: status });
+
+    // 2. Email send
+    await Query1.run({
+      status: status,
+      email: row.Email,
+      ticketId: row.TicketID
+    });
+
+  }
 }
